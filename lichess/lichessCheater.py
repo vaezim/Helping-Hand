7907f49254ed9bf51e5b258aa0baef47c7fb0f53
vaezim <mehradvaezi8@gmail.com>
2023-01-26 00:42:53 -0600

    fixed stockfish engine path

--- lichess/lichessCheater.py
+++ lichess/lichessCheater.py
@@ -43,25 +43,25 @@ bullet_button = driver.find_element(By.XPATH, "/html/body/div[1]/main/div[2]/div
 bullet_button.click()
 WAIT = 3
 for i in range(WAIT):
-	print("Starting in " + str(WAIT-i))
-	sleep(1)
+    print("Starting in " + str(WAIT-i))
+    sleep(1)
 input('Ready? >> ')
 
 # If a message box, indicating that it's our move, exists then we're white,
 # O.W. an "Unable to locate element" error is raised.
 try:
-	message = driver.find_element(By.XPATH,
-		"/html/body/div[1]/main/div[1]/rm6/div[2]")
-	COLOR = 'W'
+    message = driver.find_element(By.XPATH,
+        "/html/body/div[1]/main/div[1]/rm6/div[2]")
+    COLOR = 'W'
 except:
-	COLOR = 'B'
+    COLOR = 'B'
 
 # Creating a handle for the move input box
 move_handle = driver.find_element(By.CLASS_NAME, "ready")
 move_handle.send_keys(Keys.RETURN)
 
-# Turning on the Stockfish Engine & chess.board
-ENGINE_PATH = r"D:\Programming\@@Lichess Auto-cheater\stockfish_15_win_x64_popcnt\stockfish_15_x64_popcnt"
+# Using Stockfish Engine & chess.board
+ENGINE_PATH = r"..\stockfish_15_win_x64_popcnt\stockfish_15_x64_popcnt"
 stockfish = Stockfish(path=ENGINE_PATH, depth=18)
 stockfish.update_engine_parameters({"Hash": 2048, "Minimum Thinking Time": 100})
 stockfish.set_elo_rating(2500) # playing as a 2500 rated player
@@ -70,14 +70,14 @@ board = chess.Board()
 MOVE_NUM = 1
 
 if COLOR == 'B':
-	oppMove = driver.find_element(By.XPATH,
-		"/html/body/div[1]/main/div/rm6/l4x/u8t["+str(MOVE_NUM)+"]").text
-	UCI = board.push_san(oppMove)
-	stockfish.make_moves_from_current_position([UCI.uci()])
-	print(str(ceil(MOVE_NUM/2)) + '. ' + UCI.uci())
+    oppMove = driver.find_element(By.XPATH,
+        "/html/body/div[1]/main/div/rm6/l4x/u8t["+str(MOVE_NUM)+"]").text
+    UCI = board.push_san(oppMove)
+    stockfish.make_moves_from_current_position([UCI.uci()])
+    print(str(ceil(MOVE_NUM/2)) + '. ' + UCI.uci())
 else:
-	MOVE_NUM = 0
-	
+    MOVE_NUM = 0
+    
 myMove = stockfish.get_best_move_time(3000)
 stockfish.make_moves_from_current_position([myMove])
 print(str(ceil((MOVE_NUM+1)/2)) + '. ' + myMove)
@@ -87,21 +87,21 @@ move_handle.send_keys(myMove)
 MOVE_NUM += 2
 
 while not board.is_checkmate():
-	oppXpath = "/html/body/div[1]/main/div/rm6/l4x/u8t["+str(MOVE_NUM)+"]"
-	# Waiting until the opponent makes a move
-	while not check_exists_by_xpath(oppXpath):
-		sleep(0.3)
+    oppXpath = "/html/body/div[1]/main/div/rm6/l4x/u8t["+str(MOVE_NUM)+"]"
+    # Waiting until the opponent makes a move
+    while not check_exists_by_xpath(oppXpath):
+        sleep(0.3)
 
-	oppMove = check_exists_by_xpath(oppXpath).text
-	UCI = board.push_san(oppMove)
-	stockfish.make_moves_from_current_position([UCI.uci()])
-	print(str(ceil(MOVE_NUM/2)) + '. ' + UCI.uci())
-	myMove = stockfish.get_best_move_time(round(random.random()*2000)) # random time from [0,2]s
-	stockfish.make_moves_from_current_position([myMove])
-	print(str(ceil((MOVE_NUM+1)/2)) + '. ' + myMove)
-	board.push_uci(myMove)
-	move_handle.clear()
-	move_handle.send_keys(myMove)
-	MOVE_NUM += 2
+    oppMove = check_exists_by_xpath(oppXpath).text
+    UCI = board.push_san(oppMove)
+    stockfish.make_moves_from_current_position([UCI.uci()])
+    print(str(ceil(MOVE_NUM/2)) + '. ' + UCI.uci())
+    myMove = stockfish.get_best_move_time(round(random.random()*2000)) # random time from [0,2]s
+    stockfish.make_moves_from_current_position([myMove])
+    print(str(ceil((MOVE_NUM+1)/2)) + '. ' + myMove)
+    board.push_uci(myMove)
+    move_handle.clear()
+    move_handle.send_keys(myMove)
+    MOVE_NUM += 2
 
 print('GGs!')
